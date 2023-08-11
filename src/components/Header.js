12990af92da5f1c9d7312d63c8a14738a4898d28
fiokee <smleffiong@gmail.com>
2023-08-11 14:05:02 +0100

    setting up components

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BiBook } from 'react-icons/bi';
+import { BsChevronDown, BsToggleOn} from 'react-icons/bs';
+import { HiOutlineMoon} from 'react-icons/hi';
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='container'>
+        <h1>Dictionary</h1>
+        <div className='navbar_box'>
+        <span className='book'><BiBook/></span>
+      <ul className='navbar'>
+        <li>Serif<span><BsChevronDown/></span></li>
+        <li><span><BsToggleOn/></span></li>
+        <li><span><HiOutlineMoon/></span></li>
+      </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const Search = () => {
+    const [data, setdata] = useState("");
+    const [searchWord, setWordSearch] = useState("");
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Search
