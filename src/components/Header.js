2bb5b2b63e3e6f3cf1a500f7947368fa3587a2d3
fiokee <smleffiong@gmail.com>
2023-08-12 11:57:19 +0100

    fixing style issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BiBook } from 'react-icons/bi';
 import { BsChevronDown, BsToggleOn} from 'react-icons/bs';
 import { HiOutlineMoon} from 'react-icons/hi';
@@ -7,8 +7,9 @@ import Search from './Search';
 
 const Header = () => {
   return (
+    <Fragment>
     <div className='container'>
-        <h1>Dictionary</h1>
+        <h1>Web Dictionary</h1>
         <div className='navbar_box'>
         <span className='book'><BiBook/></span>
       <ul className='navbar'>
@@ -17,8 +18,9 @@ const Header = () => {
         <li><span><HiOutlineMoon/></span></li>
       </ul>
       </div>
-      <Search/>
     </div>
+      <Search/>
+    </Fragment>
   )
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import './search.css';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { BsFillPlayFill} from 'react-icons/bs';
+import { TbExternalLink} from 'react-icons/tb';
 import Axios from 'axios';
 
 
@@ -14,20 +16,61 @@ const Search = () => {
       Axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`
       ).then((response)=>{
         console.log(response.data[0]);
-        
+
       }).catch((error) => {
         alert("Please Enter Word:", error);
         console.error("API request error:", error);
       });
     }
   return (
-    <div>
+    <div className='box'>
         <form className="search-form" role="search" onSubmit={getWord}>
             <input type="search" className="search-input" name="q"
             placeholder="Search for any word..."
              aria-label="Search through site content" required onChange={(e)=> setWordSearch(e.target.value)}/>
                 <button onClick={getWord} type='submit' className='search-button'><AiOutlineSearch/></button>
-</form>
+        </form>
+
+        <div className='words'>
+          <h1>Keyboard</h1>
+          <span><BsFillPlayFill/></span>
+        </div>
+          <div className='phonetics'>
+          <p>/Phonetics/</p>
+          </div>
+          <div className='noun'>
+           <p>noun</p>
+           <hr/>
+        </div>
+        <div>
+          <p className='meaning'>Meaning</p>
+          <ul>
+            <li>dskfskjfsfksakfsakfkafksafdjsfkjsajfkdsakjfkjsfjk</li>
+            <li>dskfskjfsfksakfsakfkafksafdjsfkjsajfkdsakjfkjsfjk</li>
+            <li>dskfskjfsfksakfsakfkafksafdjsfkjsajfkdsakjfkjsfjk</li>
+          </ul>
+        </div>
+
+        <div className='synonyms'>
+          <span>synonyms</span>
+          <p>electronic Keyboard</p>
+        </div>
+        <div className='noun'>
+           <p>verb</p>
+           <hr/>
+        </div>
+        <div>
+        <p className='meaning'>Meaning</p>
+          <ul>
+            <li>dskfskjfsfksakfsakfkafksafdjsfkjsajfkdsakjfkjsfjk</li>
+            <p className='last-meaning'>"hgghdhdhhdhdhdhhd"</p>
+            </ul>
+        </div>
+        <hr className='horizontal'/>
+        <div className='source'>
+        <span>Source</span>
+        <p>https://rest-country-app.web.app <TbExternalLink/></p>
+        </div>
     </div>
   )
 }
