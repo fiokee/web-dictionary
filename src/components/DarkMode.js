a8deba1ac36dd623aa96623e2accea61bb1a2fa0
fiokee <smleffiong@gmail.com>
2023-08-27 19:16:59 +0100

    fixing issues on the dark theme

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -7,17 +7,26 @@ import "./DarkMode.css";
         //A function to set both dark and light mode
     const handleDarkMode = ()=>{
         document.querySelector("body").setAttribute('data-theme', 'dark')
+        localStorage.setItem("selectedTheme", "dark")
     }
 
     const handleLightMode = ()=>{
         document.querySelector("body").setAttribute('data-theme', 'light')
+        localStorage.setItem("selectedTheme", "light")
     }
+// A condition to helps when refreshing the page for default theme to set to dark
+    const selectedTheme = localStorage.getItem("selectedTheme");
+    if(selectedTheme === "dark"){
+    handleDarkMode();
+    }
+
     //function to toggle between dark and light theme
     const toggleThemeMode = (e)=>{
-    if(e.target.checked) handleDarkMode();
-    else
+    if(e.target.checked){
+     handleDarkMode();   
+    } else{
         handleLightMode();
-    
+    }
 }
     return (
         <div className='dark_mode'>
@@ -26,6 +35,7 @@ import "./DarkMode.css";
                 type='checkbox'
                 id='darkmode-toggle'
                 onChange={toggleThemeMode}
+                defaultChecked={selectedTheme==="dark"}
             />
             <label className='dark_mode_label' for='darkmode-toggle'>
                 <Sun />
